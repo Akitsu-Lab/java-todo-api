a8343aaa6a68498c11e05933fbcb49a4b83b7d97
長岡聡太 <>
2024-03-28 18:24:28 +0900

    add taskList

--- src/main/java/com/akitsulab/todo/domain/Task.java
+++ src/main/java/com/akitsulab/todo/domain/Task.java
@@ -1,6 +1,6 @@
 package com.akitsulab.todo.domain;
 
-public class task {
+public class Task {
     private int id;
     private String name;
     private int status;
---
+++ src/main/java/com/akitsulab/todo/domain/TaskList.java
@@ -0,0 +1,15 @@
+package com.akitsulab.todo.domain;
+
+import java.util.List;
+
+public class TaskList {
+    private List<Task> taskList;
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+}
